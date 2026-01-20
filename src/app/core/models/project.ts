ec609fff3b7134c0c597a22e42ff74b2a7f73758
Yusef Alaa <ouffaalaa26@gmail.com>
2026-01-20 11:19:47 +0200

    m

--- src/app/core/models/project.ts
+++ src/app/core/models/project.ts
@@ -1,2 +1,10 @@
+// core/models/project.ts
+export type ProjectStatus = 'active' | 'pending' | 'completed';
+
 export interface Project {
+  id: number;
+  name: string;
+  client: string;
+  status: ProjectStatus;
+  progress: number;
 }
