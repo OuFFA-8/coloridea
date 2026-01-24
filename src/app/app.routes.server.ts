788e159282ab0eada360771d033facf212990f07
Yusef Alaa <ouffaalaa26@gmail.com>
2026-01-24 05:04:17 +0200

    M

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'client/projects/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'admin/projects/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
+    path: 'admin/clients/:id',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
