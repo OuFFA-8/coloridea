a0261e2754aff7815a7c540fc53b7cb8c2eaf0eb
Yusef Alaa <ouffaalaa26@gmail.com>
2026-05-15 19:52:03 +0300

    refactor: simplify translation loader configuration and remove unnecessary imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,38 +1,21 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideHttpClient, HttpClient, withInterceptors } from '@angular/common/http';
-
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideTranslateService, provideTranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader, TRANSLATE_HTTP_LOADER_CONFIG } from '@ngx-translate/http-loader';
 import { authInterceptor } from './core/interceptors/auth-interceptor';
-
-// الحل هنا: الدالة مابقتش محتاجة HttpClient ومابقتش بتبعته للـ Constructor
-export function HttpLoaderFactory() {
-  return new TranslateHttpLoader();
-}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
-
-    importProvidersFrom(
-      TranslateModule.forRoot({
-        loader: {
-          provide: TranslateLoader,
-          useFactory: HttpLoaderFactory,
-          deps: [HttpClient],
-        },
-      }),
-    ),
-
+    provideTranslateService({
+      loader: provideTranslateLoader(TranslateHttpLoader),
+    }),
     {
       provide: TRANSLATE_HTTP_LOADER_CONFIG,
-      useValue: {
-        prefix: '/i18n/',
-        suffix: '.json',
-      },
+      useValue: { prefix: '/i18n/', suffix: '.json', useHttpBackend: true },
     },
   ],
 };
